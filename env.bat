5052b08b4c45d8bb9545adda7699293e22334528
bbouchahoua <bbouchahoua@gmail.com>
2013-05-07 11:27:01 +0200

    premier

---
+++ env.bat
@@ -0,0 +1,8 @@
+set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.6.0_45
+set MAVEN_HOME=C:\Bruno\02-Logiciels\Java\J2EE\apache-maven-3.0.4-bin\apache-maven-3.0.4
+set DERBY_HOME=C:\Bruno\02-Logiciels\Java\J2EE\db-derby-10.8.2.2-bin\db-derby-10.8.2.2-bin
+set JUNIT_HOME=C:\Bruno\02-Logiciels\Java\J2EE\junit4.4
+
+PATH=%PATH%;%DERBY_HOME%\bin;%MAVEN_HOME%\bin;%JAVA_HOME%\bin;%JUNIT_HOME%
+
+cd C:\Bruno\03-Developpement\Java\EE6_CHP02
---
+++ src/test/java/com/test/BookTest.java
@@ -0,0 +1,60 @@
+package com.test;
+
+import javax.persistence.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import java.sql.SQLException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.app.Book;
+
+
+public class BookTest {
+
+	
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private static EntityTransaction tx;
+	
+	
+	@BeforeClass
+	public static void initEntityManager() throws Exception {
+	emf = Persistence.createEntityManagerFactory("chapter02PU");
+	em = emf.createEntityManager();
+	}
+	@AfterClass
+	public static void closeEntityManager() throws SQLException {
+	em.close();
+	emf.close();
+	}
+	@Before
+	public void initTransaction() {
+	tx = em.getTransaction();
+	}
+	@Test
+	public void shouldCreateABook() throws Exception {
+	// Creates an instance of book
+	Book book = new Book();
+	book.setTitle("The Hitchhiker's Guide to the Galaxy");
+	book.setPrice(12.5F);
+	book.setDescription("Science fiction comedy book");
+	book.setIsbn("1-84023-742-2");
+	book.setNbOfPage(354);
+	book.setIllustrations(false);
+	// Persists the book to the database
+	tx.begin();
+	em.persist(book);
+	tx.commit();
+	assertNotNull("ID should not be null", book.getId());
+	// Retrieves all the books from the database
+	List<Book> books = 	em.createNamedQuery("findAllBooks").getResultList();
+	assertEquals(1, books.size());
+	}
+	
+}
